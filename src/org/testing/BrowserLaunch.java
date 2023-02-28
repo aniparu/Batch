6f9c52e6124b9a765a2b3b914750cdf808f32384
aniparu <aniparu122@gmail.com>
2023-02-28 19:54:57 +0530

    naveen done the Code

--- src/org/testing/BrowserLaunch.java
+++ src/org/testing/BrowserLaunch.java
@@ -21,47 +21,10 @@ public class BrowserLaunch {
 		System.out.println("Sorry for the delay");
 		System.out.println("Hai How Are today...");
 
-	
+	System.out.println("naveen work will Start");
+	int a = 23;
 		
 		
-
-	}
-	
-	
-	
-	
-	public static void main(String[] args) throws InterruptedException {
-
-		System.setProperty("webdriver.chrome.driver",
-				"C:\\Users\\anipa\\eclipse-workspace\\SeleniumBatch7Pm\\drivers\\chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
-
-		driver.get("https://www.w3schools.com/html/html_tables.asp");
-
-		driver.manage().window().maximize();
-
-		// 2.to fetch middle row middle cell
-		// table---->row----col---data
-
-		WebElement table = driver.findElement(By.xpath("//table[@id='customers']"));
-
-		List<WebElement> allRows = table.findElements(By.tagName("tr"));
-
-		for (int i = 0; i < allRows.size(); i++) {
-
-			if (i % 2 == 0) {
-				WebElement row = allRows.get(i);
-				List<WebElement> allDatas = row.findElements(By.tagName("td"));
-				for (int j = 0; j < allDatas.size(); j++) {
-					if (j % 2 == 0) {
-						WebElement data = allDatas.get(j);
-						System.out.println(data.getText());
-					}
-					
-				}
-			}
-
-		}
 
 	}
 
